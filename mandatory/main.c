2543337687f8df4d56b6f35c4ec8ee6ed691f574
Joel king yan Hii <jhii@u90z08s03.42kl.edu.my>
2022-04-21 15:48:13 +0800

    added free() in main.c

--- mandatory/main.c
+++ mandatory/main.c
@@ -6,7 +6,7 @@
 /*   By: jhii <jhii@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 14:27:03 by jhii              #+#    #+#             */
-/*   Updated: 2022/04/21 15:01:51 by jhii             ###   ########.fr       */
+/*   Updated: 2022/04/21 15:47:26 by jhii             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,6 @@ int	main(int argc, char **argv)
 		return (0);
 	table_init(&table, argc, argv);
 	philo(&table);
+	free(table.group);
 	return (0);
 }
